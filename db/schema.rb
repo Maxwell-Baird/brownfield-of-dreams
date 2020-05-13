dacbf7138a12458beaa4a981ed1258781190469e
Kevin McGrevey <niv3kmcg@gmail.com>
2020-05-13 16:04:46 -0600

    Updates user table migration to add status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_180144) do
+ActiveRecord::Schema.define(version: 2020_05_13_220010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_180144) do
     t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "token"
+    t.string "token", default: "0"
     t.string "username"
     t.string "friends", default: [], array: true
     t.string "status", default: "Inactive"
