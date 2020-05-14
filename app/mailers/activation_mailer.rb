34efd2201df269f88d36913bef03aaa518aaaf2a
Maxwell Baird <bairdm71@gmail.com>
2020-05-13 21:24:57 -0600

    fixed invite and activation

--- app/mailers/activation_mailer.rb
+++ app/mailers/activation_mailer.rb
@@ -1,5 +1,6 @@
 class ActivationMailer < ApplicationMailer
   def inform(user)
+    @user = user
     @user_first_name = user.first_name
     @user_last_name = user.last_name
     mail(to: @user.email, subject: 'Confirming your registration.')
